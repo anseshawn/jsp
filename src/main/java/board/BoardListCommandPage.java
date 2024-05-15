1fdc1076c1f7e2ede1a6f0bdc1a2fa454898700e
home <chst8937@gmail.com>
2024-05-15 21:14:18 +0900

    240515_2100 pagination

---
+++ src/main/java/board/BoardListCommandPage.java
@@ -0,0 +1,33 @@
+package board;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import common.Pagination;
+
+public class BoardListCommandPage implements BoardInterface {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		BoardDAO dao = new BoardDAO();
+		
+		// 페이징 처리 시작
+		int pag = request.getParameter("pag")==null ? 1 : Integer.parseInt(request.getParameter("pag"));
+		int pageSize = request.getParameter("pageSize")==null ? 10 : Integer.parseInt(request.getParameter("pageSize"));
+		int totRecCnt = dao.getTotRecCnt();
+		
+		int startIndexNo = Pagination.pageChange(request, pag, pageSize, totRecCnt);
+		int totPage = (totRecCnt % pageSize)==0 ? (totRecCnt / pageSize) : (totRecCnt / pageSize)+1;
+		if(pag > totPage) pag = 1;
+		// 페이징 처리 끝
+		
+		ArrayList<BoardVO> vos = dao.getBoardList(startIndexNo,pageSize);
+		
+		request.setAttribute("vos", vos);
+	}
+
+}
--- src/main/java/common/Pagination.java
+++ src/main/java/common/Pagination.java
@@ -3,7 +3,28 @@ package common;
 import javax.servlet.http.HttpServletRequest;
 
 public class Pagination {
-	public static void pageChange(HttpServletRequest request, int pag, int pageSize) {
+	public static int pageChange(HttpServletRequest request, int pag, int pageSize, int totRecCnt) {
 		// 페이지 VO 해서 메소드 만들어오기
-	}
+		
+		// 페이징 처리 시작
+		int totPage = (totRecCnt % pageSize)==0 ? (totRecCnt / pageSize) : (totRecCnt / pageSize)+1;
+		if(pag > totPage) pag = 1;
+		int startIndexNo = (pag-1) * pageSize;
+		int curScrStartNo = totRecCnt - startIndexNo;
+		int blockSize = 3;
+		int curBlock = (pag - 1) / blockSize;
+		int lastBlock = (totPage - 1) / blockSize;
+		// 페이징 처리 끝
+		
+		// 설정(지정)된 페이지의 모든 자료(변수)들을 viewPage로 넘겨줄 준비를 한다. 
+		request.setAttribute("pag", pag);
+		request.setAttribute("pageSize", pageSize);
+		request.setAttribute("totPage", totPage);
+		request.setAttribute("curScrStartNo", curScrStartNo);
+		request.setAttribute("blockSize", blockSize);
+		request.setAttribute("curBlock", curBlock);
+		request.setAttribute("lastBlock", lastBlock);
+		
+		return startIndexNo;
+	} 
 }
